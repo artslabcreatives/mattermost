fcfbb0eeae2aec891cca681896132769d1e2f667
Miguel de la Cruz <miguel@ctrlz.es>
2026-02-17 13:47:18 +0100

    Update migrations to make all index ops concurrent

--- server/channels/db/migrations/postgres/000157_add_object_type_to_property_fields.up.sql
+++ server/channels/db/migrations/postgres/000157_add_object_type_to_property_fields.up.sql
@@ -1,4 +1,1 @@
 ALTER TABLE PropertyFields ADD COLUMN IF NOT EXISTS ObjectType varchar(255) NOT NULL DEFAULT '';
-
--- Drop the old unique index that doesn't account for ObjectType
-DROP INDEX IF EXISTS idx_propertyfields_unique;
