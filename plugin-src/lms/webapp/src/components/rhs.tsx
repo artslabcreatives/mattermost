afa322116e1838694ba0104ecac23949671c2ac5
miyuru <miyuru@artslabcreatives.com>
2026-07-23 12:13:56 +0200

    Add LMS iframe plugin

---
+++ plugin-src/lms/webapp/src/components/rhs.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function RHS() {
+    return (
+        <div style={{width: '100%', height: '100%', display: 'flex', flexDirection: 'column', overflow: 'hidden'}}>
+            <iframe
+                src='https://lms.artslabcreatives.com'
+                title='LMS'
+                style={{
+                    width: '100%',
+                    height: '100%',
+                    border: 'none',
+                    flex: 1,
+                }}
+                allow='camera; microphone; clipboard-read; clipboard-write; display-capture'
+            />
+        </div>
+    );
+}
